05c9df6d160def856d1ab02da782d78b8cbc7545
Caris <cjimenez@pagatodo.com>
2020-03-17 17:07:29 -0600

    avances 17 marzo

---
+++ contabilidad.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags Seo-->
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Meta etiquetas SEO -->
+  <meta name="description" content="Asesoría de alto nivel profesional en materia Fiscal, Contable, Administrativa, Jurídica, inmobiliaria y de Capital Humano">
+  <meta name="keywords" content="asesores, fiscal, financiero, finanzas, estado financiero, solucion fiscal, asesor fiscal, asesoria fiscal, asesoria de negocios, diseño de objetivos, asesor financiero, outsourcing, planeacion fiscal, contabilidad, contable, contadores, contador, declaracion anual, tramites, impuestos, persona fisica, reclutar, reclutamiento, persona moral, contribuyente, estudio socioeconomico, control interno, administracion, recursos humanos, capital humano, capacitacion, capacitacion empresarial, laboral, contratacion, personal nuevo, busqueda de personal, inmobiliaria, inmuebles, propiedad, terreno, grupo inmobiliario, abogados, despacho de abogados, juridico, agencia de abogados, asesor juridico, juicio oral, juicio laboral, riesgo laboral, ley mercantil, seguridad social, derecho penal, derecho civil, derecho laboral, derecho fiscal, marco legal, contribuciones, contrato mercantil, contrato compraventa, compliance, seguridad laboral, defensa fiscal">
+
+  <meta name="robots" content="index, follow">
+  <meta name="author" content="Geek, Carolina Jimenez Delgado" />
+  <meta name="copyright" content="Siempre Hay Alguien, A.C." />
+
+  <link rel="canonical" href="https://asesoresgo.com/">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+  <!-- Metaetiquetas SEO -->
+
+	<!--bootstrap-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<!-- Font awesome -->
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css?family=Philosopher&display=swap" rel="stylesheet">
+
+  <!--AOS -->
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+	<title>Siempre hay alguien - A.C.</title>
+</head>
+<body>
+
+	<!--Navbar -->
+  <nav class="navbar navbar-expand-md navbar-light  d-flex align-items-start" id="menu">
+    <a class="navbar-brand" href="index.php" title="Inicio">
+      <img src="image/logo.svg" height="120" class="align-top desk" alt="asesores fiscales">
+      <img src="image/logo.svg" height="65" class="align-top movil" alt="asesores fiscales">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class=""><i class="fa fa-bars fa-2x" aria-hidden="true"></i></span>
+    </button>
+    <div class="collapse navbar-collapse pt-5" id="collapsibleNavbar"> <!-- nav-sc-item -->
+      <ul class="navbar-nav">
+        <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+          <a class="nav-link " href="index.php" title="Inicio"><span class="hover-nav">INICIO</span></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title="Servicios">
+            EVENTOS
+          </a>
+          <div class="dropdown-menu dropdown-multicol" aria-labelledby="navbarDropdown">
+            <div class="dropdown-row">
+              <a class="dropdown-item" href="contabilidad.php" title="Contabilidad">SOCIALES</a>
+              <a class="dropdown-item" href="fiscal.php" title="Fiscal">DEPORTIVOS</a>
+            </div>
+            <div class="dropdown-row">
+              <a class="dropdown-item" href="administrativo.php" title="Administrativo">CULTURALES</a>
+              <a class="dropdown-item" href="rh.php" title="Recursos Humanos">REUNIONES</a>
+            </div>
+            <div class="dropdown-row">
+              <a class="dropdown-item" href="#" title="Inmobiliaria">APOYOS</a>
+              <a class="dropdown-item" href="juridico.php" title="Juridico">TALLERES</a>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+          <a class="nav-link" href="blog.php" title="Asesores financieros"><span class="hover-nav text-dorado">BLOG</span></a>
+        </li>
+        <li class="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+          <a class="nav-link" href="index.php#contacto" title="Contacto"><span class="hover-nav text-dorado">CONTACTO</span></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- ./navbar -->
+
+
+  <!-- Quienes Somos -->
+  <section class="bg-servicios" id="nosotros">
+    <div class="container pt-5">
+      <div class="row pt-5 ">
+
+        <div class="col-md-7 pb-5">
+          <h1 class="text-dorado">Sociales</h1>
+          <p class="text-dorado text-justify">
+            Dentro del municipio destacan diferentes festividades y fiestas patronales a las que se les otorga difusión y promoción para que toda la comunidad asista, entre las principales fiestas patronales destacan las siguientes.
+          </p>
+          <div class="row">
+            <div class="col-md-6">
+              <ul class="text-dorado">
+                <li>Carnaval
+                  <ul>
+                    <li>Cabecera Municipal</li>
+                    <li>Atlapulco</li>
+                  </ul>
+                </li>
+                <li>Fiesta Patronal de Ocoyoacac</li>
+                <li>Fiesta de San Pedro</li>
+                <li>Fiesta de San Pablo</li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+              <ul class="text-dorado">
+                <li>Semana Santa
+                  <ul>
+                    <li>Atlapulco</li>
+                    <li>Cholula</li>
+                    <li>Coapanoaya</li>
+                    <li>Tepexoyuca</li>
+                  </ul>
+                </li>
+                <li>Fiesta de San Miguel</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-5 pb-5" data-aos="zoom-in-down">
+          <img src="image/contabilidad-servicio.png" class="desk" alt="asesoria contable" width="100%">
+          <img src="image/contabilidad-servicio.png" class="movil img-fluid" alt="asesoria contable">
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- ------------- -->
+
+  <footer class="bg-doradolight">
+    <div class="container">
+      <div class="col-md-11 mx-auto text-center pt-5 ap">
+        <a href="index.php" class="text-bluedark pr-2 deskF" title="Home">Inicio</a>
+        <span class="ml-5 mr-5 deskC text-bluedark">/</span>
+        <a href="index.php#nosotros" class="text-bluedark pr-2 deskF" title="Nosotros">Nosotros</a>
+        <span class="ml-5 mr-5 deskC text-bluedark">/</span>
+        <a href="index.php#servcios" class="text-bluedark pr-2 deskF" title="Servicios">Eventos</a>
+        <span class="ml-5 mr-5 deskC text-bluedark">/</span>
+        <a href="index.php#contacto" class="text-bluedark pr-2 deskF" title="Contacto">Contacto
+        </a>
+        <a href="" class="text-bluedark pr-2 deskF" title="FaceBook">
+          <img src="image/face-icon-azul.svg" width="30">
+          <!--<i class="fa fa-facebook" aria-hidden="true"></i>-->
+        </a>
+
+        <p class="text-white">
+          <br>
+          <a class="text-bluelight" href="aviso-de-privacidad.php" title="Aviso de Privacidad"> AVISO DE PRIVACIDAD </a>
+          <br>
+        </p>
+      </div>
+      <div class="row mx-auto">
+        <div class="col-md-12 text-center">
+          <p class="text-bluedark font-min"> Siempre hay Alguien Ocoyoacac A.C Derechos Reservados 2020</p>
+        </div>
+        <div class="col-md-12 text-center">
+          <a class="text-bluedark font-min" href="https://ztark.mx/" title="Desarrollado por Geek">Powered By Geek</a>
+        </div>
+      </div>
+
+      <h1 style="opacity: 0;font-size: 8px;margin-bottom: 0;">
+      </h1>
+    </div>
+  </footer>
+
+
+
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('.carousel').carousel({
+        interval: 4000
+      })
+    });
+  </script>
+
+  <!--AOS efectos fade -->
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+      AOS.init({
+        easing: 'ease-out-back',
+        duration: 3000,
+        anchorPlacement: 'top-bottom',
+      });
+  </script>
+
+</body>
+</html>
